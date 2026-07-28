820fd40297a6c3dfc507e1d60ce3566514254f41
Zoe <agreen1990@gmail.com>
2026-07-28 12:23:18 -0500

    Update load.py

--- load.py
+++ load.py
@@ -11,15 +11,18 @@ def load_data_to_neon():
     print("Connecting to Neon Database...")
 
     #1. Grab your connection string from the environment variables
-    db_url = os.environ.get("postgresql://neondb_owner:npg_04rxtPLYEKcq@ep-quiet-rice-axate7ft.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require")
+    db_url = os.environ.get("DATABASE_URL")
 
     #2. Read the CSV that your script created in the Transform step
     df = pd.read_csv(transformed_movies.csv)
 
     #3. Establish a database connection
+    engine = create_engine(db_url)
+
+    #4 Write DataFrame directly to a SQL table named 'movies'
     df.to_sql("movies", con=engine, if_exists="replace", index=False)
 
     print("Succes: TMDB data safely into Neon!")
-    
+
 if __name__ == "__main__":
     load_data_to_neon()
